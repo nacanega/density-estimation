7fb49eeaf258f7f364427c206bb028147e760f47
Nolan Canegallo <nacanega@ncsu.edu>
2024-12-04 00:40:09 -0500

    SNC Gamma matrix functions

---
+++ state_equations/gammaROT_Ns1_Nm.m
@@ -0,0 +1,17 @@
+function [Gamma,varagout] = gammaROT_Ns1_Nm(dt,state)
+%gammaROT_7s returns the Gamma and Rotation matrices for a 7 state system
+nargoutchk(1,2)
+
+N = length(state);
+I = eye(3);
+Gamma = zeros(N,3);
+Gamma(1:6,:) = dt*[(dt/2)*I;I];
+
+if nargout == 2
+    rs = state(1:3)'; r = sqrt(rs.'*rs); rhat = rs./r;
+    vs = state(4:6)'; v = sqrt(vs.'*vs); vhat = vs./v;
+    nhat = cross(-rhat,vhat);
+    varagout{1} = [-rhat,vhat,nhat];
+end
+
+end
---
+++ state_equations/gammaROT_NsN_Nm.m
@@ -0,0 +1,28 @@
+function [Gamma,varargout] = gammaROT_NsN_Nm(dt,state,nSatStates,nModStates)
+%gammaROT_7s returns the Gamma and Rotation matrices for a 7 state system
+nargoutchk(1,2)
+
+N = length(state);
+nSats = (N - nModStates) / nSatStates;
+
+Gamma = zeros(N,3*nSats);
+
+if nargout == 2
+    gamma = zerosCell([nSats,1],[nSatStates,3]);
+    rotms = zerosCell([nSats,1],[3,3]);
+    for i = nSats:-1:1
+        iState = state(nSatStates*(i-1)+1:nSatStates*i);
+        [gamma{i},rotms{i}] = gammaROT_Ns1_Nm(dt,iState);
+    end
+    Gamma(1:end-mModStates,:) = sparse(blkdiag(gamma{:}));
+    varargout = sparse(blkdiag(rotms{:}));
+else
+    gamma = zerosCell([nSats,1],[nSatStates,3]);
+    for i = nSats:-1:1
+        iState = state(nSatStates*(i-1)+1:nSatStates*i);
+        gamma{i} = gammaROT_Ns1_Nm(dt,iState);
+    end
+    Gamma(1:end-mModStates,:) = sparse(blkdiag(gamma{:}));
+end
+
+end
